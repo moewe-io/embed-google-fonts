ca7c7eaa95f5ca750e15e007e6374730e0c394d5
Adrian Mörchen <adrian.moerchen@moewe.io>
2018-05-15 16:35:16 +0200

    Initial commit

---
+++ embed-google-fonts.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Plugin Name: Embed Google Fonts
+ * Plugin URI: https://github.com/moewe-io/embed-google-fonts
+ * Description: Helper plugin for embedding Google fonts.
+ * Version: 1.0.0
+ * Author: MOEWE
+ * Author URI: https://www.moewe.io/
+ * Text Domain: embed-google-fonts
+ */
+
+add_action('wp_enqueue_scripts', 'embed_google_fonts_enqueue', 0);
+
+function embed_google_fonts_enqueue() {
+    $base_url = plugins_url() . '/replace-google-fonts/fonts/';
+    wp_register_style('rgf-open-sans', $base_url . 'open-sans.css', array(), '15');
+
+    // Lucid Theme
+    wp_enqueue_style('google_font_open_sans', $base_url . 'open-sans.css', array(), '15');
+    wp_enqueue_style('google_font_open_sans_condensed', $base_url . 'open-sans.css', array(), '15');
+}
+
+/**
+ * Replacement for Elegant Themes
+ */
+if (!function_exists('et_gf_enqueue_fonts')) {
+    function et_gf_enqueue_fonts($et_gf_font_names) {
+        foreach ($et_gf_font_names as $et_gf_font_name) {
+            $et_gf_font_name_slug = strtolower(str_replace(' ', '-', $et_gf_font_name));
+            wp_enqueue_style('rgf-' . $et_gf_font_name_slug);
+        }
+    }
+}
+
+// Updates
+
+require 'libs/plugin-update-checker-4.4/plugin-update-checker.php';
+Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/moewe-io/embed-google-fonts/',
+    __FILE__,
+    'embed-google-fonts'
+)->setBranch('master');
---
+++ readme.txt
@@ -0,0 +1,22 @@
+=== Embed Google Fonts ===
+Contributors: adrian2k7
+Tags: quform
+Donate link: https://www.moewe.io/
+Requires at least: 4.0
+Tested up to: 4.6
+Stable tag: 1.0.0
+License: GPL v3
+License URI: http://www.gnu.org/copyleft/gpl.html
+
+Quick and dirty helper plugin for embedding Google fonts, basically developed for us and our customers.
+
+== Description ==
+Quick and dirty helper plugin for embedding Google fonts, basically developed for us and our customers.
+
+**Note** This doesn't automatically replaces all your Google fonts with local versions, only what we've programmed in.
+
+== Changelog ==
+
+= 1.0.0 =
+
+* Initial release
