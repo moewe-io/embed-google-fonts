480569088d4fd6e6884458bd63dfc99d7dfd119d
Adrian Mörchen <adrian.moerchen@moewe.io>
2018-05-15 16:51:50 +0200

    Moved Open Sans CSS into folder

--- embed-google-fonts.php
+++ embed-google-fonts.php
@@ -4,7 +4,7 @@
  * Plugin Name: Embed Google Fonts
  * Plugin URI: https://github.com/moewe-io/embed-google-fonts
  * Description: Helper plugin for embedding Google fonts.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: MOEWE
  * Author URI: https://www.moewe.io/
  * Text Domain: embed-google-fonts
@@ -14,7 +14,7 @@ add_action('wp_enqueue_scripts', 'embed_google_fonts_enqueue', 0);
 
 function embed_google_fonts_enqueue() {
     $base_url = plugins_url() . '/replace-google-fonts/fonts/';
-    wp_register_style('rgf-open-sans', $base_url . 'open-sans.css', array(), '15');
+    wp_register_style('rgf-open-sans', $base_url . 'open-sans/open-sans.css', array(), '15');
 
     // Lucid Theme
     wp_enqueue_style('google_font_open_sans', $base_url . 'open-sans.css', array(), '15');
@@ -34,7 +34,6 @@ if (!function_exists('et_gf_enqueue_fonts')) {
 }
 
 // Updates
-
 require 'libs/plugin-update-checker-4.4/plugin-update-checker.php';
 Puc_v4_Factory::buildUpdateChecker(
     'https://github.com/moewe-io/embed-google-fonts/',
